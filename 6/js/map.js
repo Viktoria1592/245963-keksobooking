d670e7de3d2bfb1b60646a3a80d16771d23e851a
Keks <keks@htmlacademy.ru>
2017-12-14 23:31:14 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/map.js
@@ -0,0 +1,125 @@
+'use strict';
+
+// модуль, который работает с картой
+(function () {
+
+  var ENTER_KEYCODE = 13;
+  var noticeForm = document.querySelector('.notice__form');
+  var address = noticeForm.querySelector('#address');
+  var formFieldset = document.querySelectorAll('.fieldset');
+  var map = document.querySelector('.map'); // общая поле = карта + настройки
+  var mapPinMain = map.querySelector('.map__pin--main');
+  var mapPins = map.querySelector('.map__pins'); // находим элемент-карту в которую отрисовываем сгенерированные DOM-элементы
+  var locationMainInForm = { // координаты главного маркера-пина
+    x: 580,
+    y: 355
+  };
+  var pinWidth = 40; // ширина иконки
+  var pinHeight = 40; // высота иконки пина
+
+  // функция внесения адрес-координат в форму по умолчанию - для тестирования формы
+  var getAddress = function () {
+    address.value = locationMainInForm.x + ', ' + locationMainInForm.y;
+  };
+
+  /*
+  // Закрыть попап объявления по умолчанию
+  var closePopup = function () {
+    articleElement.classList.add('hidden');
+  };
+  var openPopup = function () {
+    articleElement.classList.remove('hidden');
+  };
+  */
+
+  // функция делает недоступными все поля форм по умолчанию
+  var getDisabledMapAndForms = function () {
+    map.classList.add('map--faded');
+    for (var j = 0; j < formFieldset.length; j++) {
+      formFieldset[j].setAttribute('disabled', 'disabled');
+    }
+  };
+  getDisabledMapAndForms();
+
+  // событие активирует карту и форму
+  var getActivateMapAndForms = function () {
+    mapPins.appendChild(window.pin.fragment); // добавленте маркеров на карту, хранящихся в fragment
+    map.classList.remove('map--faded');
+    noticeForm.classList.remove('notice__form--disabled');
+    window.card.renderArticle(window.card.arrayOfAds[0]); // отрисовываем 1й попап по умолчанию в общий map перед блоком map__filters-container
+    for (var j = 0; j < formFieldset.length; j++) {
+      formFieldset[j].removeAttribute('disabled', 'disabled');
+    }
+    getAddress();
+  };
+
+  // обработчик события на блоке при отпускании кнопки мыши активирует поля и карту
+  mapPinMain.addEventListener('mouseup', getActivateMapAndForms);
+
+  // обработчик события на блоке при нажатии ENTER
+  mapPinMain.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      getActivateMapAndForms();
+    }
+  });
+
+  // обработчик события закрытия попапа и убирает активный класса при ESC
+  document.addEventListener('keydown', window.card.onPopupEscPress);
+
+  // перетаскивание
+  mapPinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMainPinDrag = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var newX = mapPinMain.offsetLeft - shift.x; // переменная для отображения положения текущего пина по оси абсцисс
+      if (newX > window.data.locationXY.maxX) { // ограничения для пина на крте по оси абсцисс
+        mapPinMain.style.left = '900 px';
+        locationMainInForm.x = window.data.locationXY.maxX;
+      } else if (newX < window.data.locationXY.minX) {
+        mapPinMain.style.left = '300 px';
+        locationMainInForm.x = window.data.locationXY.minX;
+      } else {
+        mapPinMain.style.left = newX + 'px'; // отображение текущей координаты х
+        locationMainInForm.x = newX - pinWidth / 2; // отображение координат для вывода в форму
+      }
+
+      var newY = mapPinMain.offsetTop - shift.y; // переменная для отображения положения текущего пина по оси ординат
+      if (newY > window.data.locationXY.maxY) { // ограничения для пина на крте по оси ординат
+        mapPinMain.style.top = '500 px';
+        locationMainInForm.y = window.data.locationXY.maxY;
+      } else if (newY < window.data.locationXY.minY) {
+        mapPinMain.style.top = '100 px';
+        locationMainInForm.y = window.data.locationXY.minY;
+      } else {
+        mapPinMain.style.top = newY + 'px'; // отображение текущей координаты у
+        locationMainInForm.y = newY - pinHeight / 2; // отображение координат для вывода в форму
+      }
+
+      getAddress(); // вывод текущих координат главного пина в форму
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMainPinDrag);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    document.addEventListener('mousemove', onMainPinDrag);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})();
---
+++ 6/js/pin.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// модуль для отрисовки пина и взаимодействия с ним, без вставки
+(function () {
+
+  // Находим шаблон маркера в template, который будем копировать
+  var mapPinTemplate = document.querySelector('template').content.querySelector('.map__pin');
+  var ENTER_KEYCODE = 13;
+  var pinWidth = 40; // ширина иконки
+  var pinHeight = 40; // высота иконки
+
+  // функция учитывает ширину картинки, смещается влево на пол-ширину
+  var getPinWidth = function (x) {
+    return x - pinWidth / 2;
+  };
+
+  // функция учитывает высоту маркера и высоту картинки
+  var getPinHeight = function (y) {
+    return y - pinHeight;
+  };
+
+  // Создает DOM-элемент маркера на основе шаблона и данных объявления
+  var renderPoint = function (ads) {
+    var pinElement = mapPinTemplate.cloneNode(true); // клонируем содержимое маркера из template
+    pinElement.querySelector('img').width = pinWidth;
+    pinElement.querySelector('img').height = pinHeight;
+    pinElement.style.left = getPinWidth(ads.location.x) + 'px';
+    pinElement.style.top = getPinHeight(ads.location.y) + 'px';
+    pinElement.querySelector('img').src = ads.author.avatar;
+    pinElement.tabIndex = 1;
+    pinElement.className = 'map__pin'; // задал имя классу
+
+    // обработчик событий замены акивного маркера по клику и появление своего попапа
+    pinElement.addEventListener('click', function () {
+      window.card.removeActive(); // снимает активный класс у кого находит при клике на маркер
+      window.card.hideArticle(); // скрывает текущий попап
+      pinElement.classList.add('map__pin--active');
+      window.card.renderArticle(ads); // отрисовка объявления-попапа соответствующего нажатому маркеру
+    });
+
+    // обработчик событий замены акивного маркера по клику и появление своего попапа при нажатии Enter
+    pinElement.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        window.card.removeActive();
+        window.card.hideArticle();
+        pinElement.classList.add('map__pin--active');
+        window.card.renderArticle(ads);
+      }
+    });
+    return pinElement;
+  };
+
+  // добавляем сгенерированный DOM-элемент маркера в fragment и храним его там. Добавляем на карту только при нажатии кнопки-активации
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < window.card.arrayOfAds.length; i++) {
+    fragment.appendChild(renderPoint(window.card.arrayOfAds[i])); // рендорим в массив-объявлений маркеры, каждому объявлению по маркеру
+  }
+
+  // перенос в глобальную область видимости
+  window.pin = {
+    fragment: fragment
+  };
+})();

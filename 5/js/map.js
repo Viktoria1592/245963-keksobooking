240cac7f0f85f8ccf9c6beae78d3309f59eb837c
Keks <keks@htmlacademy.ru>
2017-12-14 14:19:31 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/map.js
+++ 5/js/map.js
@@ -10,14 +10,16 @@
   var map = document.querySelector('.map'); // общая поле = карта + настройки
   var mapPinMain = map.querySelector('.map__pin--main');
   var mapPins = map.querySelector('.map__pins'); // находим элемент-карту в которую отрисовываем сгенерированные DOM-элементы
-  var locationMain = { // координаты главного маркера-пина
-    x: 600,
-    y: 380
+  var locationMainInForm = { // координаты главного маркера-пина
+    x: 580,
+    y: 355
   };
+  var pinWidth = 40; // ширина иконки
+  var pinHeight = 40; // высота иконки пина
 
   // функция внесения адрес-координат в форму по умолчанию - для тестирования формы
   var getAddress = function () {
-    address.value = locationMain.x + ', ' + locationMain.y;
+    address.value = locationMainInForm.x + ', ' + locationMainInForm.y;
   };
 
   /*
@@ -63,4 +65,61 @@
 
   // обработчик события закрытия попапа и убирает активный класса при ESC
   document.addEventListener('keydown', window.card.onPopupEscPress);
+
+  // перетаскивание
+  mapPinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var newX = mapPinMain.offsetLeft - shift.x; // переменная для отображения положения текущего пина по оси абсцисс
+      if (newX > window.data.locationXY.maxX) { // ограничения для пина на крте по оси абсцисс
+        mapPinMain.style.left = '900 px';
+        locationMainInForm.x = window.data.locationXY.maxX;
+      } else if (newX < window.data.locationXY.minX) {
+        mapPinMain.style.left = '300 px';
+        locationMainInForm.x = window.data.locationXY.minX;
+      } else {
+        mapPinMain.style.left = newX + 'px'; // отображение текущей координаты х
+        locationMainInForm.x = newX - pinWidth / 2; // отображение координат для вывода в форму
+      }
+
+      var newY = mapPinMain.offsetTop - shift.y; // переменная для отображения положения текущего пина по оси ординат
+      if (newY > window.data.locationXY.maxY) { // ограничения для пина на крте по оси ординат
+        mapPinMain.style.top = '500 px';
+        locationMainInForm.y = window.data.locationXY.maxY;
+      } else if (newY < window.data.locationXY.minY) {
+        mapPinMain.style.top = '100 px';
+        locationMainInForm.y = window.data.locationXY.minY;
+      } else {
+        mapPinMain.style.top = newY + 'px'; // отображение текущей координаты у
+        locationMainInForm.y = newY - pinHeight / 2; // отображение координат для вывода в форму
+      }
+
+      getAddress(); // вывод текущих координат главного пина в форму
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
 })();
